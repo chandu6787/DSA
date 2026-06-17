36e521c9236dd64f07f164d10160c85af8d51684
chandu6787 <159985349+chandu6787@users.noreply.github.com>
2026-06-17 18:32:11 +0530

    Time: 148 ms (97.4%), Space: 20.7 MB (42.43%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,18 +1,19 @@
 class Solution:
-    def required_time(self,piles,speed):
-        total_hours=0
+    def total_hours(self,speed,piles):
+        hours_sum=0
         for pile in piles:
-            total_hours+=ceil(pile/speed)
-        return total_hours
+            hours_sum+=ceil(pile/speed)
+        return hours_sum
+
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         low,high=1,max(piles)
         while low<=high:
             mid=(low+high)//2
-            if self.required_time(piles,mid)>h:
-                low=mid+1
-            elif self.required_time(piles,mid)<=h:
+            if self.total_hours(mid,piles)<=h:
                 high=mid-1
+            else:
+                low=mid+1
+
         return low
         
-
         
