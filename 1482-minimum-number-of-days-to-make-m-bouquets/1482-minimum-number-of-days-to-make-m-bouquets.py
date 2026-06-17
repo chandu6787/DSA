dac750f70a08fac2c8403f9c6344a3239fd5c51f
chandu6787 <159985349+chandu6787@users.noreply.github.com>
2026-06-17 18:55:18 +0530

    Time: 263 ms (96.38%), Space: 32.4 MB (12.93%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -1,31 +1,30 @@
 class Solution:
-    def possibility(self,bloomDay,assumeDay,m,k):
-        cnt=0
-        noofBouq=0
-        for day in bloomDay:
-            if day<=assumeDay:
-                cnt+=1
+    def possible(self,bloomDay,day,m,k):
+        count_flowers=0
+        bouques=0
+        for item in bloomDay:
+            if item<=day:
+                count_flowers+=1
             else:
-                noofBouq+=(cnt)//k
-                cnt=0
-        noofBouq+=(cnt)//k
-        if noofBouq>=m:
+                bouques+=(count_flowers//k)
+                count_flowers=0
+        bouques+=(count_flowers//k)
+        if bouques>=m:
             return True
         else:
             return False
 
+
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
         low,high=min(bloomDay),max(bloomDay)
-        ans=-1
+        if m*k>len(bloomDay):
+            return -1
         while low<=high:
             mid=(low+high)//2
-            if self.possibility(bloomDay,mid,m,k)==True:
+            if self.possible(bloomDay,mid,m,k):
                 high=mid-1
             else:
                 low=mid+1
-        if low<=max(bloomDay):
-            return low
-        else:
-            return -1
 
+        return low
         
