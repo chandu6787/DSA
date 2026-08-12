cdad1f5bba24a90979167353384dacca3abbb788
chandu6787 <159985349+chandu6787@users.noreply.github.com>
2026-08-12 08:30:12 +0530

    Time: 11 ms (38.18%), Space: 23.7 MB (98.97%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -6,14 +6,14 @@
 #         self.right = right
 class Solution:
     def recu(self,root):
-      if root is None:
-        return 0
-      left = max(0,self.recu(root.left))
-      right = max(0,self.recu(root.right))
-      self.ans=max(self.ans,left+right+root.val)
-      return root.val+max(left,right)
+        if root is None:
+            return 0
+        left=max(0,self.recu(root.left))
+        right=max(0,self.recu(root.right))
+        self.maxi=max(self.maxi,root.val+left+right)
+        return root.val+max(left,right)
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        self.ans=float('-inf')
+        self.maxi=float('-inf')
         self.recu(root)
-        return self.ans
+        return self.maxi
         
